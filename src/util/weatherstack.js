c63d4c3ce5013888c334928e6889a549f558c6d2
Muhammad Hassan <fearless10494@gmail.com>
2022-09-02 17:03:53 +0500

    Added more funtionality

--- src/util/weatherstack.js
+++ src/util/weatherstack.js
@@ -3,7 +3,7 @@
 const request = require('postman-request'); // to retrive data from api
 
 const wheatherStack = (longitude, latitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=3d15d443ffe8e56220c27bf5369c4e17&query=${longitude},${latitude}`;
+    const url = `http://api.weatherstack.com/forecast?access_key=3d15d443ffe8e56220c27bf5369c4e17&query=${longitude},${latitude}`;
 
     // json arg will automatically parse the response
     request({ url: url, json: true }, (error, { body }) => {
@@ -12,8 +12,10 @@ const wheatherStack = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
+            console.log(body);
             const data = body.current;
-            callback(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees outside. It feels like ${data.feelslike}. There is ${data.precip}% chance of rain.`);
+
+            callback(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees outside. It feels like ${data.feelslike}. The humidity is ${data.humidity}%. There is a ${data.precip}% chance of rain.`);
         }
     });
 }
